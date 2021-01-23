19d03b1b207a025e0b71ee9011e94920473b91f0
Haf <haf@protonmail.ch>
2021-01-23 17:25:06 +0100

    first configuration I'm satisfied with

---
+++ home.nix
@@ -0,0 +1,103 @@
+{ config, pkgs, ... }:
+let
+  unstable = import <nixos-unstable> { config = { allowUnfree = true; }; };
+in
+  {
+    imports = [
+      ./nvim.nix
+      ./urxvt.nix
+    ];
+
+  # Let Home Manager install and manage itself.
+  nixpkgs.config.allowUnfree = true;
+  programs.home-manager.enable = true;
+  programs.bash.enable = true;
+  programs.fzf.enable = true;
+  programs.fzf.enableBashIntegration = true;
+
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "haf";
+  home.homeDirectory = "/home/haf";
+  home.sessionVariables.EDITOR = "nvim";
+
+  home.packages = with pkgs; [
+    postman
+    i3status
+    terminus_font
+    anki
+    zathura
+    (texlive.combine {
+      inherit (texlive) scheme-basic cancel;
+    })
+    scrot
+    jetbrains.idea-ultimate
+    unstable.neuron-notes
+
+    # for classes
+    simplescreenrecorder
+    google-chrome
+    teams
+  ];
+
+  nixpkgs.config.packageOverrides = pkgs: {
+    nur = import (builtins.fetchTarball "https://github.com/nix-community/NUR/archive/master.tar.gz") {
+      inherit pkgs;
+    };
+  };
+
+  programs.firefox = {
+    enable = true;
+    profiles = {
+      default = {
+        isDefault = true;
+      };
+    };
+    extensions = with pkgs.nur.repos.rycee.firefox-addons; [
+      ublock-origin
+    ];
+  };
+
+  xsession = {
+    enable = true;
+    windowManager.i3 = {
+      enable = true;
+      config.modifier = "Mod4";
+      config.window.titlebar = false;
+      config.terminal = "alacritty";
+      config.menu = "rofi -show run";
+    };
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      font.size = 10;
+    };
+  };
+
+  programs.git = {
+    enable = true;
+    userName = "Haf";
+    userEmail = "haf@protonmail.ch";
+  };
+
+  programs.rofi = {
+    enable = true;
+    theme = "dmenu";
+  };
+
+  programs.emacs = {
+    enable = true;
+  };
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.03";
+}
---
+++ nvim.nix
@@ -0,0 +1,41 @@
+{pkgs, ...}:
+
+{
+  programs.neovim = {
+    enable = true;
+    extraConfig = ''
+      set expandtab " insert spaces on tab
+      set tabstop=2 " number of spaces inserted on tab
+      set shiftwidth=2 " number of spaces used for indentation
+      set mouse=a " enable mouse
+      set conceallevel=2 " hides information (using on vim-markdown)
+      set number " show line numbers
+      set scrolloff=5 " always show five lines above and below cursor
+
+      augroup vimStartup
+      au!
+
+      " When editing a file, always jump to the last known cursor position.
+      " Don't do it when the position is invalid, when inside an event handler
+      " (happens when dropping a file on gvim) and for a commit message (it's
+      " likely a different one than last time).
+      autocmd BufReadPost *
+      \ if line("'\"") >= 1 && line("'\"") <= line("$") && &ft !~# 'commit'
+      \ |   exe "normal! g`\""
+      \ | endif
+
+      augroup END
+    '';
+
+    plugins = with pkgs.vimPlugins; [
+      vim-nix
+      vim-markdown
+      vim-surround
+      neuron-vim
+      fzf-vim
+      coc-nvim
+    ];
+  };
+
+  xdg.configFile."nvim/coc-settings.json".text = builtins.readFile ./my-coc-settings.json;
+}
