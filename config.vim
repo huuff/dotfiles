af6f60e41c6c0566a742de8de84c858c849588e6
Haf <haf@protonmail.ch>
2021-04-16 00:53:56 +0200

    added snippets, separated vim file

---
+++ config.vim
@@ -0,0 +1,37 @@
+set expandtab " insert spaces on tab
+set tabstop=2 " number of spaces inserted on tab
+set shiftwidth=2 " number of spaces used for indentation
+set mouse=a " enable mouse
+set conceallevel=2 " hides information (using on vim-markdown)
+set number " show line numbers
+set scrolloff=5 " always show five lines above and below cursor
+set wrap linebreak nolist " wrap lines
+
+" use space as leader
+nnoremap <SPACE> <Nop>
+let mapleader=" "
+
+" start with cursor at last known position
+augroup vimStartup
+au!
+
+autocmd BufReadPost *
+\ if line("'\"") >= 1 && line("'\"") <= line("$") && &ft !~# 'commit'
+\ |   exe "normal! g`\""
+\ | endif
+
+augroup END
+
+" tab for trigger completion, completion confirm, snippet expand and jump like VSCode
+inoremap <silent><expr> <TAB>
+      \ pumvisible() ? coc#_select_confirm() :
+      \ coc#expandableOrJumpable() ? "\<C-r>=coc#rpc#request('doKeymap', ['snippets-expand-jump',''])\<CR>" :
+      \ <SID>check_back_space() ? "\<TAB>" :
+      \ coc#refresh()
+
+function! s:check_back_space() abort
+  let col = col('.') - 1
+  return !col || getline('.')[col - 1]  =~# '\s'
+endfunction
+
+let g:coc_snippet_next = '<tab>'
--- nvim.nix
+++ nvim.nix
@@ -3,35 +3,8 @@
 {
   programs.neovim = {
     enable = true;
-    extraConfig = ''
-      set expandtab " insert spaces on tab
-      set tabstop=2 " number of spaces inserted on tab
-      set shiftwidth=2 " number of spaces used for indentation
-      set mouse=a " enable mouse
-      set conceallevel=2 " hides information (using on vim-markdown)
-      set number " show line numbers
-      set scrolloff=5 " always show five lines above and below cursor
-      set wrap linebreak nolist " wrap lines
 
-      " use space as leader
-      nnoremap <SPACE> <Nop>
-      let mapleader=" "
-
-      augroup vimStartup
-      au!
-
-      " When editing a file, always jump to the last known cursor position.
-      " Don't do it when the position is invalid, when inside an event handler
-      " (happens when dropping a file on gvim) and for a commit message (it's
-      " likely a different one than last time).
-      autocmd BufReadPost *
-      \ if line("'\"") >= 1 && line("'\"") <= line("$") && &ft !~# 'commit'
-      \ |   exe "normal! g`\""
-      \ | endif
-
-      augroup END
-    '';
-
+    extraConfig = builtins.readFile ./config.vim;
     plugins = with pkgs.vimPlugins; [
       vim-nix
       vim-surround
@@ -39,6 +12,7 @@
       mkdx
       fzf-vim
       coc-nvim
+      coc-snippets
       nerdcommenter
       fugitive
       vim-gitgutter
