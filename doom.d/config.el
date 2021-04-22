324672a3968d8afd70134833c2f7176d247f2f00
Haf <haf@protonmail.ch>
2021-04-23 01:47:35 +0200

    changed centaurtabs tab size, added little comments

--- doom.d/config.el
+++ doom.d/config.el
@@ -1,6 +1,12 @@
+; open links in firefox
 (setq browse-url-browser-function 'browse-url-firefox 
       browse-url-new-window-flag t)
 
+; press C-x 7 to open next buffer in a new frame
 (other-frame-window-mode)
 
+; just set a bigger font
 (set-face-attribute 'default nil :height 110)
+
+; fixed size for tabs since dynamic fucks everything up
+(setq centaur-tabs-label-fixed-length 18)
