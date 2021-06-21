61331531eef415431815dcf2c8e1831464f78fcf
Haf <haf@protonmail.ch>
2021-06-21 17:09:19 +0200

    removing fzf shell aliases

--- home.nix
+++ home.nix
@@ -34,7 +34,6 @@ in
   programs.home-manager.enable = true;
 
   programs.fzf.enable = true;
-  #programs.fzf.enableBashIntegration = true;
 
   haf.scripts.enable = true;
   haf.autocutsel.enable = true;
